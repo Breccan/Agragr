db39e26a439e37a8be60be735e69ac7cc94e9076
Breccan <jalnyfen@gmail.com>
2010-10-16 13:03:56 +1300

    Initial generate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atn::Application.config.secret_token = '47c95e16da36d04d6085f9e67a60c27b50d8622a1fa6e8bb9929c48688f9cb46ab7f9f74441ce91f8928c748cf72245d083a871d7f2d8fe313d2876927f5db8c'
