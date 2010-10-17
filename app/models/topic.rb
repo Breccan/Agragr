28f21806164dce625d8079c724206cd63f0a8a3a
Breccan <jalnyfen@gmail.com>
2010-10-17 18:21:47 +1300

    Fixed disabled

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,6 +3,6 @@ class Topic < ActiveRecord::Base
   has_many :links
 
   def disabled(session_values)
-    session_values["topics"].include(self.id.to_s)
+    session_values["topics"].include?(self.id)
   end
 end
