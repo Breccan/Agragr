8b0c473edfebd5bfa63c051f501dbe832d9cdc95
Yuffster <msteigerwalt@gmail.com>
2010-10-17 03:20:29 +0000

    Added a domain attribute to links.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,14 @@
 class Link < ActiveRecord::Base
+
   belongs_to :topic
   has_many :listings
+
+  def domain
+    match = self.url.match(/http[s]?:\/\/([^\/]*)/)
+    return nil if !match
+    subs = match[1].split('.')
+    subs.shift() if subs[0] == 'www'
+    subs.join('.')
+  end
 
 end
