01fb46b17cb6d7cfdb640ff0ec503cfc46a3bbc2
Yuffster <msteigerwalt@gmail.com>
2010-10-17 07:19:25 +0000

    Filters.

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,8 +3,15 @@ class Link < ActiveRecord::Base
   belongs_to :topic
   has_many :listings
 
-  Filters = {"NSFW" => 1, "Self" => 2, "Image" => 3, "Youtube" => 4, "does anyone else..." => 5,
-             '"My dog", "My cat"' => 6}
+  Filters = {
+    :images  => 'images',
+    :youtube => "youtube",
+    :dae     => '"does anyone else..."',
+    :pets    => '"my dog", "my cat"',
+    :dating  => '"my girlfriend..."',
+    :tweets  => 'headline only',
+    :nsfw    => 'nsfw',
+  }
 
   scope :exlude_nsfw, lambda {
     joins(:listings => :reddit_listings).where(["reddit_listings.nsfw = ?", false])
@@ -19,7 +26,6 @@ class Link < ActiveRecord::Base
     where("links.topic_id IN (#{topic_ids.join(', ')})")
   }
 
-
   def self.build_filter_scope(prefs)
     combined_scope = order('links.created_at DESC')
     combined_scope = load_filter_scopes(prefs[:filters], combined_scope)
@@ -27,13 +33,10 @@ class Link < ActiveRecord::Base
   end
 
   def self.load_filter_scopes(filters, combined_scope)
+    return combined_scope
     filters.each do |filter|
-      case filter
-      when 1
-        combined_scope = combined_scope.exlude_nsfw
-      when 2
-        combined_scope = combined_scope.exlude_self
-      end
+      k = 'exclude_#{filter}'
+      combined_scope = combined_scope[k]
     end
    combined_scope 
   end
