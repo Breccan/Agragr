43ca83e005d804166f98616f705ee88908b72580
Yuffster <msteigerwalt@gmail.com>
2010-10-17 04:01:49 +0000

    Seed code.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,20 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
+['worldnews', 'science', 'tech', 'programming', 'entertainment', 'funny', 'community', 'hobbies'].each do |topic|
+    Topic.create(:name => topic)
+end
+
 [
   {
     :name => "Hacker News",
     :url => "http://new.ycombinator.com",
-    :harvest_strategy => "hn"
+    :harvest_strategy => "hn",
+    :topic_name => 'tech'
   },
-  { :name => "R/Programming",
+  { :name => "r-programming",
     :url => 'http://www.reddit.com/r/programming.json',
+    :topic_name => 'programming',
     :harvest_strategy => "reddit" 
   }
 ].each do |source|
@@ -22,3 +28,39 @@
   end
 end
 
+#subreddits and their topics
+{
+
+  :worldnews => [
+
+  ],
+
+  :programming => [
+    'programming', 'ruby', 'python', 'java', 'php', 'javascript', 'jquery'
+  ],
+
+  :funny => [
+    'pics', 'jokes', 'funny'
+  ],
+
+  :science => [
+
+  ],
+
+  :hobbies => [
+
+  ],
+
+  :tech => [
+
+  ],
+
+  :entertainment => [
+
+  ],
+
+  :community => [
+
+  ]
+
+}
